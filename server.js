b30af900f298c7d49b8d463aeab4bc6388047b58
serversideapps <serversideapps2@gmail.com>
2018-01-28 16:27:46 +0100

    Initial commit js

---
+++ server.js
@@ -0,0 +1,19 @@
+"use strict";
+const express = require('express');
+const mongodb = require('mongodb');
+const app = express();
+let PORT = 8080;
+let MONGODB_URI = process.env.MONGODB_URI;
+console.log(`mongo uri ${MONGODB_URI}`);
+let db = null;
+mongodb.connect(MONGODB_URI, function (err, conn) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        db = conn;
+        console.log(`connected to MongoDB database < ${db.databaseName} >`);
+    }
+});
+app.get('/', (req, res) => res.send('<b>Welcome to livote!</b> Lichess shadow app.'));
+app.listen(PORT, () => console.log(`livote server listening on ${PORT}`));
